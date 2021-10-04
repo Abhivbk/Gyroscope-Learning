453e6e83d3e4e27139501de5bda068677e1de524
Abhinav Kulkarni <abhivbk13@gmail.com>
2021-10-04 19:51:07 +0530

    Initial Commit

---
+++ Graph.py
@@ -0,0 +1,27 @@
+from itertools import count
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+
+plt.style.use('fivethirtyeight')
+
+x_values = []
+y_values = []
+
+index = count()
+
+def animate(i):
+    with open('orientation.txt', 'r') as f:
+        roll = f.read()
+    x_values.append(next(index))
+    y_values.append(roll)
+    print(x_values)
+    print(y_values)
+    plt.cla()
+    plt.plot(x_values, y_values, marker='o')
+
+
+ani = FuncAnimation(plt.gcf(), animate, 1000)
+
+
+plt.tight_layout()
+plt.show()
